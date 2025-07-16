8b05af1b5ee4a6bec6d9ebbd117bd727e5b20a95
wyrtwala <89209191+wyrtwala@users.noreply.github.com>
2025-07-15 19:52:39 -0700

    Update cast.h

--- lib/cast.h
+++ lib/cast.h
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <inttypes.h>
 #include "lexer.h"
-#include "error.h"
 
 
 
 uint64_t* get_cast(FILE* input) {
+  uint64_t mask = 0;
+  uint64_t mark_runner = 0;
   
 
   
