b7921c343ae4c4b031d9189651692f2bf6930527
wyrtwala <89209191+wyrtwala@users.noreply.github.com>
2025-07-16 16:45:19 -0700

    Update cast.h
    added double pointer

--- lib/cast.h
+++ lib/cast.h
@@ -5,10 +5,10 @@
 
 
 
-uint64_t* get_cast(FILE* input) {
+char** get_cast(FILE* input) {
   uint64_t mask = 0;
   uint64_t mark_runner = 0;
-  
+  char** cast = (char**)malloc(64 * sizeof(char*));
 
   
 }
