2bc58e91d963ddbf75c841eb2c718e84edb3c44d
wyrtwala <89209191+wyrtwala@users.noreply.github.com>
2025-07-16 18:04:10 -0700

    Update cast.h
    more setup

--- lib/cast.h
+++ lib/cast.h
@@ -6,9 +6,45 @@
 
 
 char** get_cast(FILE* input) {
-  uint64_t mask = 0;
-  uint64_t mark_runner = 0;
-  char** cast = (char**)malloc(64 * sizeof(char*));
+  //setup
+  uint64_t mask        = 1;         //bitwise map of cast
+  uint64_t ast_runner  = 1;         //current ast depth :: for cutoff at d64
+  uint64_t cast_runner = 1;         //current cast depth
+  uint64_t tok_count   = 1;
+  uint64_t par_count   = 0;
+  char** cast          = (char**)malloc(64 * sizeof(char*));
+  token cur_tok        = get_tok(input); 
+  if (cur_tok.TOK == TOK_LPARA || cur_tok.TOK == TOK_RPARA) {
+    log_error("top level logic cannot start with ()");
+    ablinnan();
+  }
+  cast[0] = cur_tok.ELEMENT;
 
-  
+  //pop loop
+  while (cur_tok.TOK != TOK_EOF) {
+    /*
+                                    1
+                    2                                 3
+            4                5                6                7
+        8       9      10        11      12        13      14      15
+    */
+
+    // n*2+1
+    if (cur_tok.TOK != TOK_LPARA && cur_tok.TOK != TOKLPARA) {
+      
+
+
+
+
+      
+    } else {
+
+      
+    }
+  if (cur_tok.TOK != TOK_EOF) {
+    log_error("")
+    ablinnan();
+  }
+  return cast;
+  }
 }
