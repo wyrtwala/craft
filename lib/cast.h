0bce9eddcb47cd60cb14066702355fe7423c8202
wyrtwala <89209191+wyrtwala@users.noreply.github.com>
2025-07-16 19:47:15 -0700

    Update cast.h
    begin tree

--- lib/cast.h
+++ lib/cast.h
@@ -3,15 +3,18 @@
 #include <inttypes.h>
 #include "lexer.h"
 
+typedef struct Branch {
+  uint64_t mask;
+  char** branch
+} Branch;
 
 
 char** get_cast(FILE* input) {
   //setup
   uint64_t mask        = 1;         //bitwise map of cast
-  uint64_t ast_runner  = 1;         //current ast depth :: for cutoff at d64
-  uint64_t cast_runner = 1;         //current cast depth
+  uint64_t cast_runner = 2;         //current cast position
+  uint64_t par_count   = 0;         //current ast depth :: for cutoff at d64 :: checked with '('
   uint64_t tok_count   = 1;
-  uint64_t par_count   = 0;
   char** cast          = (char**)malloc(64 * sizeof(char*));
   token cur_tok        = get_tok(input); 
   if (cur_tok.TOK == TOK_LPARA || cur_tok.TOK == TOK_RPARA) {
@@ -22,22 +25,20 @@ char** get_cast(FILE* input) {
 
   //pop loop
   while (cur_tok.TOK != TOK_EOF) {
-    /*
-                                    1
-                    2                                 3
-            4                5                6                7
-        8       9      10        11      12        13      14      15
-    */
+    cur_tok = get_tok(input);
 
-    // n*2+1
     if (cur_tok.TOK != TOK_LPARA && cur_tok.TOK != TOKLPARA) {
-      
+      if (cast_runner % 3 != 0 && cast_runner % 2 != 0) { //primary node
+        cast[cast_runner - 1] = cur_tok.ELEMENT;
+        cast_runner++;
+      }
+      if (tok_count % 2 != 0 && 
 
 
 
 
       
-    } else {
+    } else { //parentheses :: reshuffle
 
       
     }
