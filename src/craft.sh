d2f56471689f4fcd70051f04ddbcc6203ce730ad
wyrtwala <89209191+wyrtwala@users.noreply.github.com>
2025-09-02 05:00:20 +0000

    Update craft.sh

--- src/craft.sh
+++ src/craft.sh
@@ -1,42 +1,41 @@
-#!/bin/usr/env bash
+#!/usr/bin/env bash
 
- 
+
 ########################################################################################################
 # SETUP
 ########################################################################################################
-infile=0   # 0 = stdin | 1 = file
-outfile=0  # 0 = stdout | 1 = file
-compilef=0 # 0 = assembly, 1 = compile, 2 = library
-for arg in $@ 
-  case $arg in
-    "-i" | "--input"   | "input"  ); shift 1; infile=1; instring=$(<$arg); ;;
-    "-c" | "--compile" | "compile"); $compile=1; ;; 
-    "-l" | "--library" | "library"); $compile=2; ;;
-    "-o" | "--output"  | "output" ); shift 1; outfile=1; output_file=$(<$arg); ;;
-    "-d" | "--define"  | "define" ); shift 1; define $arg; exit; ;;
-    "-h" | "help"      | "--help" );
-      echo "craft [input] [type] [output]"
-      echo "  -i, --input    : input filepath"
+compile=3
+inputstring=$(cat)
+outputfile="/dev/stdout"
+#DEFINE ASSEMBLY
+function define() {
+case $1 in
+  "link to dict") echo "definition"; ;;
+  *) echo "not linked to dictionary yet"
+esac
+}
+
+case $1 in
+    "-a" | "--assembly" | "assembly") compile=0; ;;
+    "-c" | "--compile"  | "compile" ) compile=1; ;; 
+    "-l" | "--library"  | "library" ) compile=2; ;;
+    "-o" | "--output"   | "output"  ) outputfile=$2 ;;
+    "-d" | "--define"   | "define"  ) define $2; exit; ;;
+    "-h" | "help"       | "--help"  )
+      echo "craft format|define|help [output filepath]"
+      echo "  -a, --assembly : output in assembly code"
       echo "  -c, --compile  : output in machine code"
       echo "  -l, --library  : output assembly in library format"
       echo "  -o, --output   : output to file"
       echo "  -d, --define   : print craft command"
-      echo "                   assembly translation to stdout"
       exit
     ;;
-  esac
+                                    *) echo "sorry I don't understand that instruction"; exit; ;;
+esac
 
-
-########################################################################################################
-# FUNCTIONS
-########################################################################################################
-
-
-
-
-
-
+if [[ compile -eq 3 ]]; then echo "sorry, i need to know what format you would like to use"; exit; fi;
 
 ########################################################################################################
 # MAIN
 ########################################################################################################
+echo $inputstring
