3dd81febf01c70a26f011f09eefd59571025f101
wyrtwala <89209191+wyrtwala@users.noreply.github.com>
2025-09-04 20:02:47 -0700

    Update craft.py

--- src/craft.py
+++ src/craft.py
@@ -1,12 +1,40 @@
 #!/bin/usr/env python3
-
-
-# MAIN
-def craft():
-
-# END MAIN
+# IMPORTS
+import sys
+# GLOBALS
+input_string = ''
+output_file = sys.stdout
+output_type = 0
+# FUNCTIONS
+def setup():
+    if sys.argc < 2:
+        print('craft needs at least two arguments')
+        sys.exit()
+    match sys.argv[1]:
+        case "-a" | "--assembly" | "assembly":
+            output_type = 1
+        case "-c" | "--compile"  | "compile":
+            output_type = 2
+        case "-l" | "--library"  | "library":
+            output_type = 3
+        case "-d" | "--define"   | "define":
+            craft_define(sys.argv[2])
+            sys.exit()
+        case "-h" | "help"       | "--help":
+            print("craft format|define|help input_file [output_file]"
+                  "  -a, --assembly : output in assembly code"
+                  "  -c, --compile  : output in machine code"
+                  "  -l, --library  : output assembly in library format"
+                  "  -d, --define   : print craft command"
+                  "  -h, --help     : display this message")
+        
 
 
 # RUN Craft
 if __name__ == "__main__":
-    craft()
+    setup()
+    tokenize()
+    parse()
+    assemble()
+    compile()
+    end()
