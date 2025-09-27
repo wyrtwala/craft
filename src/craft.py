8571d10567bad0622cda542bc1ba570c8eaf694a
wyrtwala <89209191+wyrtwala@users.noreply.github.com>
2025-09-27 15:51:36 -0700

    Update craft.py
    working out the bugs

--- src/craft.py
+++ src/craft.py
@@ -1,29 +1,14 @@
 #!/bin/usr/env python3
 
-
 #######################################################################
 # IMPORTS
 #######################################################################
 import sys
 import os
 
-
-#######################################################################
-# GLOBALS
-#######################################################################
-input_file  = None
-input       = None
-dictionary  = None
-output_file = "/dev/stdout"
-output_type = 0
-
-#######################################################################
-# LISTS
-#######################################################################
-runtime_defs = []
-# garmr        = []
-# huginn       = []
-# muginn       = []
+dictionary = open("dictionary.txt", 'r')
+input      = None
+output     = None
 
 
 #######################################################################
@@ -38,24 +23,20 @@ class Token:
     def set_type(self, type):
         self.type = type
 
-    
 #######################################################################
 # FUNCTIONS
 #######################################################################
-def craft_define(word):
+def define(word):
     count, definition = check_dictionary(word)
     if count != -1:
-        print(f"{word} does exist, it takes {count} arguments and is defined as: \n{definition}")
+        print(f"{word} takes {count} arguments and is defined as: \n{definition}")
         sys.exit(0)
     else:
-        print{f"{word} does not exist")
+        print(f"{word} has not been defined")
         sys.exit(0)
 
-
 def setup():
-    if sys.argc < 2:
-        print('craft needs at least two arguments')
-        sys.exit(1)
+    global output_type
     match sys.argv[1]:
         case "-a" | "--assembly" | "assembly":
             output_type = 1
@@ -63,67 +44,102 @@ def setup():
             output_type = 2
         case "-l" | "--library"  | "library":
             output_type = 3
+        case "-u" | "--unlinked" | "unlinked":
+            output_type = 4
         case "-d" | "--define"   | "define":
-            craft_define(sys.argv[2])
+            define(sys.argv[2])
             sys.exit()
         case "-h" | "help"       | "--help":
-            print("craft format|define|help input_file [output_file]"
-                  "  -a, --assembly : output in assembly code"
-                  "  -c, --compile  : output in machine code"
-                  "  -l, --library  : output assembly in library format"
-                  "  -d, --define   : print craft command"
-                  "  -h, --help     : display this message")
+            print("craft format|define|help input_file output_file\n"
+                  "  -a, --assembly : output in assembly code\n"
+                  "  -c, --compile  : output in machine code\n"
+                  "  -u, --unlinked : output unlinked binary\n"
+                  "  -l, --library  : output assembly in library format\n"
+                  "  -d, --define   : print craft command\n"
+                  "  -h, --help     : display this message\n")
         case _:
-            print("craft doesn't understand that argument")
-            print("craft format|define|help input_file [output_file]",
-                  "  -a, --assembly : output in assembly code",
-                  "  -c, --compile  : output in machine code",
-                  "  -l, --library  : output assembly in library format",
-                  "  -d, --define   : print craft command",
-                  "  -h, --help     : display this message")
+            print("craft doesn't understand that argument\n")
+            print("craft format|define|help input_file output_file\n",
+                  "  -a, --assembly : output in assembly code\n",
+                  "  -c, --compile  : output in machine code\n",
+                  "  -u, --unlinked : output unlinked binary\n"
+                  "  -l, --library  : output assembly in library format\n",
+                  "  -d, --define   : print craft command\n",
+                  "  -h, --help     : display this message\n")
             sys.exit(1)
-    input_file = sys.argv[2]
-    while open(input_file, 'rb') as input
-    if input.closed:
-        print("craft couldn't open file")
+    if len(sys.argv) < 3:
+        print("craft needs an output file")
         sys.exit(1)
-    dictionary = open("dictionary.txt", 'rb')
+    input = open(sys.argv[2], 'rb')
+    if input.closed:
+        print("craft couldn't open input file")
+        sys.exit(1)
+    output = open(sys.argv[3], 'rb')
+    if output.closed:
+        print("craft couldn't open output file")
+        sys.exit(1)
     if dictionary.closed:
         print("craft couldn't open dictionary")
         sys.exit(1)
     if output_type == 0:
         print("craft needs to know in what format to output your code")
         sys.exit(1)
-    if output_type == 3 and sys.argc < 3:
-        print("craft needs an output file for the library")
-        sys.exit(1)
-    if sys.argc == 3:
-        output_file = sys.argv[3]
 
 
 def compile_craft():
     if output_type == 1:
-        print(assembly_string, output_file)
+        print(assembly_string, output)
+        os.exit(0)
     elif output_type == 2:
-        asm = open("craft.asm", 'w')
-        if asm.closed:
-            print("craft couldn't open output file\n")
-            sys.exit(2)
-        print(assembly_string, asm)
-        compile_command = "gcc -c craft.s -o " + output_file
+        print(assembly_string, output)
+        compile_command = "gcc craft.s -o " + sys.argv[2]
         os.system(compile_command)
-        close(asm)
-        os.remove("craft.asm")
+        sys.exit(0)
     elif output_type == 3:
-        lib = open(output_file, 'w')
-        if lib.closed:
-            print("craft couln't open library file\n")
-            sys.exit(2)
-        print("%ifndef ", output_file, "\n%define ", output_file, "\n\n\n", file=lib)
-        print(assembly_string, file=lib)
-        print("%endif \n\n", file=lib)
+        print("[library]", output)
+        print(dictionary, output)
+        print("[library]", output)
+        print(assembly_string, output)
+        sys.exit(0)
+    elif output_type == 4:
+        print(assembly_string, output)
+        compile_command = "gcc -c craft.s -o " + sys.argv[2]
+        os.system(compile_command)
+        sys.exit(0)
 
 
+
+def check_dictionary(word):
+    for line in dictionary:
+        words = line.strip().split()
+        if words[0] == word:
+            return words[1], ' '.join(words[2:])
+        else:
+            pass
+    return -1, str(-1)
+
+
+#######################################################################
+# TENTATIVE
+#######################################################################
+
+def translate(): # possibly use instead of parse
+    for line in input:
+        words = line.strip().split()
+
+
+
+#######################################################################
+# CRAFT MAIN
+#######################################################################
+if __name__ == "__main__":
+    setup()
+    #parse()
+    compile_craft()
+    sys.exit(0)
+######################################################################
+# YA MAYBE NOT
+######################################################################
 def next_tok():
     next = Token()
     word = []
@@ -168,7 +184,7 @@ def next_tok():
                         next.set_type(4)
                         return next
             case _:
-                word.append(x):
+                word.append(x)
                 while True:
                     x = input.read(1)
                     if x.isspace() | b'':
@@ -179,17 +195,6 @@ def next_tok():
                         word.append(x)
 
 
-def check_dictionary(word):
-    for line in dictionary:
-        line = line.strip().split()
-        if line[0] == word:
-            return ord(line[1]) ' '.join(line[2:])
-    return -1, str(-1)
-
-
-#######################################################################
-# TENTATIVE
-#######################################################################
 
 def parse():
     cur_tok = Token()
@@ -201,7 +206,7 @@ def parse():
         if count < 0:
             print("at line {line_count}: {cur_tok.word} is not defined")
             sys.exit(3)
-        elif count = 0:
+        elif count == 0:
             assembly_string += definition
         else:
             var_list = []
@@ -211,20 +216,4 @@ def parse():
                     print("at {line_count} {cur_tok.word} expected {count} arguments, but only recieved {i + 1}")
                     sys.exit(3)
             assembly_string += definition.format(var_list[:])
-        #TODO
-
-def translate(): # possibly use instead of parse
-    for line in input:
-        words = line.strip().split()
-
-
-
-#######################################################################
-# CRAFT MAIN
-#######################################################################
-if __name__ == "__main__":
-    setup()
-    parse()
-    compile_craft()
-    sys.exit(0)
-
+        pass
